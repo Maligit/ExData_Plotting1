6cd9ce4adf27c3a63cbe2789ce290aea487f4082
Maligit <Maligit@users.noreply.github.com>
2017-03-05 22:12:03 -0500

    Add files via upload

---
+++ readPowerData.R
@@ -0,0 +1,5 @@
+readPowerData <- function(){
+  inputFile <- "./household_power_consumption.txt"
+  powerData <- read.table(inputFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+  subPowerData <- powerData[powerData$Date %in% c("1/2/2007","2/2/2007") ,]
+}
